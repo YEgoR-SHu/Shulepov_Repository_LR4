aeb69bda917d8496655c7a2da3f092159461f221
YEgoR-SHu <shulepovegor3003@gmail.com>
2025-03-25 23:58:37 +0300

    new project

---
+++ Surname_project_LR4.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// Структура для хранения чисел
+struct Numbers {
+    int x, y, z;
+};
+
+// Функции для ввода значений X, Y, Z
+void getX(Numbers& nums) {
+
+}
+
+void getY(Numbers& nums) {
+
+}
+
+void getZ(Numbers& nums) {
+
+}
+
+// Функция для определения количества четных чисел
+void CountChet(const Numbers& nums) {
+
+}
+
+// Функция для вычисления суммы четных чисел
+void SumChet(const Numbers& nums) {
+
+}
+
+// Функция для отображения меню
+void showMenu() {
+    cout << "\nMenu:\n";
+    cout << "1. Ввести число X\n";
+    cout << "2. Ввести число Y\n";
+    cout << "3. Ввести число Z\n";
+    cout << "4. Определить количество четных чисел\n";
+    cout << "5. Найти сумму четных чисел\n";
+    cout << "0. Выход\n";
+    cout << "Выберите пункт меню: ";
+}
+
+int main() {
+    Numbers nums = {0, 0, 0};
+    int choice;
+
+    while (true) {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                getX(nums);
+                break;
+            case 2:
+                getY(nums);
+                break;
+            case 3:
+                getZ(nums);
+                break;
+            case 4:
+                CountChet(nums);
+                break;
+            case 5:
+                SumChet(nums);
+                break;
+            case 0:
+                cout << "Выход из программы." << endl;
+                return 0;
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+        }
+    }
+    return 0;
+}
